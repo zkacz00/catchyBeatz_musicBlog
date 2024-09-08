e6d9e9f82fb233538c5bd8fdf1f56760d0790260
zkacz00 <zk.zofia.kaczmarczyk@gmail.com>
2024-09-08 21:48:38 +0200

    changes in style

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,13 +10,32 @@ const config: GatsbyConfig = {
     author: `@z_kacz`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    }
-  }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-transformer-sharp", "gatsby-image", "gatsby-transformer-remark" ]
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      }
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-image",
+    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `catchy vibez`,
+        short_name: `catchyVibez`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/favicon/favicon.png`,
+      },
+    },
+  ]
 };
 
 export default config;
